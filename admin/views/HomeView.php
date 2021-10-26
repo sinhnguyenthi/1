ebbeab4f37034637830b02c6bcd0b3e09fe839bf
nguyenthisinh <nguyenthisinh07051999@gmail.com>
2021-10-26 14:23:36 +0700

    del

--- admin/views/HomeView.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	// load file layout.php vao day
-	$this->fileLayout = "Layout.php";
-?>
-<h3> Home view</h3>
--- controllers/ContactController.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	class ContactController extends Controller{
-		public function index(){
-			$this->loadView("ContactView.php");
-		}
-	}
-?>
